fc40dfc54adb7c57ba79ecc4e74ca2720c1141e1
Mamitiana1437 <mamitiana_rakotoarisoa@yahoo.com>
2021-03-10 18:31:20 +0300

    Initial commit

---
+++ model/assignment.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+let aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+let Schema = mongoose.Schema;
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+AssignmentSchema.plugin(aggregatePaginate);
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+module.exports = mongoose.model('Assignment', AssignmentSchema);
